9439e0df5cb661db7b310a23e213bd86f69ca178
rudoxe <125541105+rudoxe@users.noreply.github.com>
2023-04-20 14:26:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function handleSubmit(event) {
+    event.preventDefault();
+    console.log("💣palaista");
+}
+
+// function kvadrata(x){
+//     let x2 = x+y;
+//     console.log (x2);
+// }
+// kvadrata(2);
+
+
+function saskaiti(x,y){
+    let x2 = x+y;
+    console.log("Summa ir ", x+y);
+}
+saskaiti(2);
